404e09b9c5d50dd1378b071b76fe0292197dc101
Chen-AMao <ansi6@126.com>
2018-10-06 21:46:43 +0800

    add header

--- travel-app/src/main.js
+++ travel-app/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+// 解决移动端300ms延迟的点击问题
+fastClick.attach(document.body)
 
 new Vue({
   router,
