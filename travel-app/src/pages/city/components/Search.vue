ab4986c3d642659443890a35b29ca7caf5a7a50d
Chen-AMao <ansi6@126.com>
2018-10-20 17:45:04 +0800

    city-search

---
+++ travel-app/src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音" >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~@/assets/styles/varibles.styl';
+    .search
+        height: 1.72rem
+        padding: .2rem .5rem
+        background: $bgColor
+        .search-input
+            box-sizing: border-box
+            width: 100%
+            height: 1.62rem
+            padding: 0 .5rem
+            line-height: 1.62rem
+            text-align: center
+            border-radius: .06rem
+            color: #666
+            font-size: .72rem
+</style>
+
+
